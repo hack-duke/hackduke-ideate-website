4d2addae7cbdbd6f5c3c59eb69122c168c9330fc
Mitchell Kim <mckim0928@gmail.com>
2017-02-26 17:01:43 -0500

    fixing linting errors

--- src/components/Workshops/Workshops.js
+++ src/components/Workshops/Workshops.js
@@ -11,15 +11,31 @@ export const Workshops = () => (
         <Row>
           <Col md={6}>
             <WorkshopSection title={'Workshop 1'} presenter={'Presenter'}
-              text={'Lorem ipsum dolor sit amet, sint delicata electram eos at. Eu mundi legendos eam. Esse ancillae eos ea, nam perfecto comprehensam at, an quo lorem labitur propriae. Decore munere singulis ei usu, eu vel accusam evertitur, ne agam sanctus intellegat mei.'} />
+              text={'Lorem ipsum dolor sit amet, sint delicata electram eos at.\
+               Eu mundi legendos eam. Esse ancillae eos ea, nam perfecto \
+               comprehensam at, an quo lorem labitur propriae. Decore munere \
+               singulis ei usu, eu vel accusam evertitur, ne agam sanctus \
+               intellegat mei.'} />
             <WorkshopSection title={'Workshop 2'} presenter={'Presenter'}
-              text={'Lorem ipsum dolor sit amet, sint delicata electram eos at. Eu mundi legendos eam. Esse ancillae eos ea, nam perfecto comprehensam at, an quo lorem labitur propriae. Decore munere singulis ei usu, eu vel accusam evertitur, ne agam sanctus intellegat mei.'} />
+              text={'Lorem ipsum dolor sit amet, sint delicata electram eos at.\
+               Eu mundi legendos eam. Esse ancillae eos ea, nam perfecto \
+               comprehensam at, an quo lorem labitur propriae. Decore munere \
+               singulis ei usu, eu vel accusam evertitur, ne agam sanctus \
+               intellegat mei.'} />
           </Col>
           <Col md={6}>
             <WorkshopSection title={'Workshop 3'} presenter={'Presenter'}
-              text={'Lorem ipsum dolor sit amet, sint delicata electram eos at. Eu mundi legendos eam. Esse ancillae eos ea, nam perfecto comprehensam at, an quo lorem labitur propriae. Decore munere singulis ei usu, eu vel accusam evertitur, ne agam sanctus intellegat mei.'} />
+              text={'Lorem ipsum dolor sit amet, sint delicata electram eos at.\
+               Eu mundi legendos eam. Esse ancillae eos ea, nam perfecto \
+               comprehensam at, an quo lorem labitur propriae. Decore munere \
+               singulis ei usu, eu vel accusam evertitur, ne agam sanctus \
+               intellegat mei.'} />
             <WorkshopSection title={'Workshop 4'} presenter={'Presenter'}
-              text={'Lorem ipsum dolor sit amet, sint delicata electram eos at. Eu mundi legendos eam. Esse ancillae eos ea, nam perfecto comprehensam at, an quo lorem labitur propriae. Decore munere singulis ei usu, eu vel accusam evertitur, ne agam sanctus intellegat mei.'} />
+              text={'Lorem ipsum dolor sit amet, sint delicata electram eos at.\
+               Eu mundi legendos eam. Esse ancillae eos ea, nam perfecto \
+               comprehensam at, an quo lorem labitur propriae. Decore munere \
+               singulis ei usu, eu vel accusam evertitur, ne agam sanctus \
+               intellegat mei.'} />
           </Col>
         </Row>
       </div>
