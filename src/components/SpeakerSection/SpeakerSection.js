bc801b87d44b12612dbf54685278d46d82dd8bae
Mitchell Kim <mckim0928@gmail.com>
2017-03-01 23:53:16 -0500

    add placeholder values. need to adjust styles

--- src/components/SpeakerSection/SpeakerSection.js
+++ src/components/SpeakerSection/SpeakerSection.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import classes from './SpeakerSection.scss'
-import {Col} from 'react-bootstrap'
+import {Row} from 'react-bootstrap'
 
 export const SpeakerSection= (props) => (
 
   <div>
+    <Row className={classes.imageSection}>
+      <div className={classes.image}>
 
+      </div>
+    </Row>
+    <Row className={classes.contentSection}>
+      <div className={classes.sectionHeader}>
+        {props.name}
+      </div>
+      <div className={classes.sectionText}>
+        {props.text}
+      </div>
+    </Row>
   </div>
 )
 
--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import classes from './Speakers.scss'
 import SpeakerSection from 'components/SpeakerSection'
+import {Col,Row} from 'react-bootstrap'
+
+const title ='SPEAKERS'
 
 export const Speakers = () => (
   <div>
     <div className={classes.speakers}>
       <div className={'container text-center'}>
-        <h1 className={classes.header}>Speakers</h1>
-
+        <h1 className={classes.header}>{title}</h1>
+        <Row>
+          <Col md={6}>
+            <SpeakerSection name={'Speaker 1'}
+              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+              Pellentesque vestibulum tellus et sagittis feugiat. Suspendisse \
+              eu sapien sed nisl facilisis fermentum.'}/>
+          </Col>
+          <Col md={6}>
+            <SpeakerSection name={'Speaker 1'}
+              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+              Pellentesque vestibulum tellus et sagittis feugiat. Suspendisse \
+              eu sapien sed nisl facilisis fermentum.'}/>
+          </Col>
+        </Row>
       </div>
     </div>
   </div>
