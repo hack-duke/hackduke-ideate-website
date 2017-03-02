8bd3dd1e6ce865e0cbf81d353e4eced7c41eb350
Mitchell Kim <mckim0928@gmail.com>
2017-03-01 23:37:00 -0500

    add backbones for speaker section components

---
+++ src/components/SpeakerSection/SpeakerSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './SpeakerSection.scss'
+import {Col} from 'react-bootstrap'
+
+export const SpeakerSection= (props) => (
+
+  <div>
+
+  </div>
+)
+
+SpeakerSection.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  text: React.PropTypes.string.isRequired
+}
+
+export default SpeakerSection
