9d36763dfe3d5cc88b1ad09d36af54fb7cb50adb
Mitchell Kim <mckim0928@gmail.com>
2017-03-04 17:05:41 -0500

    fix linting errors

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -6,8 +6,8 @@ export const HeroPage = () => (
     <div className={classes.hero}>
       <div className={classes.mainText}>
         <h1 className={classes.header}>IDEATE</h1>
-        <h3 className={classes.subheader}>The student innovation & design conference <br/>
-            April 15, 2017 @ Duke University</h3>
+        <h3 className={classes.subheader}>The student innovation & design conference <br />
+            April 15, 2017 @ Duke University </h3>
         <div className={classes.buttonDiv}>
           <a className={classes.registerLink} eventKey={1} href={'http://my.hackduke.org/login'}>
             <button className={classes.registerButton}> Register </button>
