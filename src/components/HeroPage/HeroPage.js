984cd7f2ecbba817d385dce9d1affa6a4f742b74
Mitchell Kim <mckim0928@gmail.com>
2017-03-04 17:03:53 -0500

    play around with the hero page

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -5,15 +5,12 @@ export const HeroPage = () => (
   <div>
     <div className={classes.hero}>
       <div className={classes.mainText}>
-        <img src='hackduke-logo.png' alt='Hackduke Logo' />
-        <h6>November 19-20, 2016</h6>
+        <h1 className={classes.header}>IDEATE</h1>
+        <h3 className={classes.subheader}>The student innovation & design conference <br/>
+            April 15, 2017 @ Duke University</h3>
         <div className={classes.buttonDiv}>
-          <a className={classes.applyLink} eventKey={1} href={'http://my.hackduke.org/login'}>
-            <button className={classes.loginButton}> Login </button>
-          </a>
-          <a className={classes.applyLink}
-            href={'https://hackduke.typeform.com/to/pZb8zj?route_receive_participant=xxxxx'}>
-            <button className={classes.homeButton}> Apply Now </button>
+          <a className={classes.registerLink} eventKey={1} href={'http://my.hackduke.org/login'}>
+            <button className={classes.registerButton}> Register </button>
           </a>
         </div>
       </div>
