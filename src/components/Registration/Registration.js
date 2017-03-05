a218b10cb3b069c1d73579080dc60ae3ed1efd0f
Gideon Pfeffer <gap16@duke.edu>
2017-03-05 17:02:33 -0500

    readded react

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import regclasses from '../Registration/Registration.scss'
 
-const title = 'Registration'
-
 export const Registration = () => (
   <div>
-      <div className ={regclasses.ready}>
-      	Ready to go?
-      </div>
-      <div className = {regclasses.register}>
-      	Register Now
+    <div className ={regclasses.ready}>
+      Ready to go?
+    </div>
+    <div className = {regclasses.register}>
+      Register Now
     </div>
   </div>
-)
+ )
 
 export default Registration
