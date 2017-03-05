5a4f2fa517874aa69575f4df21e6e67b9a5b351b
Mitchell Kim <mckim0928@gmail.com>
2017-03-05 14:26:33 -0500

    removing container center-text for correct alignment

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -8,24 +8,22 @@ const title = 'SPEAKERS'
 export const Speakers = () => (
   <div>
     <div className={classes.speakers}>
-      <div className={'container text-center'}>
-        <h1 className={classes.header}>{title}</h1>
-        <div className={classes.sectionContainer}>
-          <Row>
-            <Col md={6}>
-              <SpeakerSection name={'Person 1'}
-                text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-                Pellentesque vestibulum tellus et sagittis feugiat. Suspendisse \
-                eu sapien sed nisl facilisis fermentum.'} />
-            </Col>
-            <Col md={6}>
-              <SpeakerSection name={'Person 1'}
-                text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-                Pellentesque vestibulum tellus et sagittis feugiat. Suspendisse \
-                eu sapien sed nisl facilisis fermentum.'} />
-            </Col>
-          </Row>
-        </div>
+      <h1 className={classes.header}>{title}</h1>
+      <div className={classes.sectionContainer}>
+        <Row>
+          <Col md={6}>
+            <SpeakerSection name={'Person 1'}
+              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+              Pellentesque vestibulum tellus et sagittis feugiat. Suspendisse \
+              eu sapien sed nisl facilisis fermentum.'} />
+          </Col>
+          <Col md={6}>
+            <SpeakerSection name={'Person 1'}
+              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+              Pellentesque vestibulum tellus et sagittis feugiat. Suspendisse \
+              eu sapien sed nisl facilisis fermentum.'} />
+          </Col>
+        </Row>
       </div>
     </div>
   </div>
