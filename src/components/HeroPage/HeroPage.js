179e8a404b4ed045bd30aa92159ac84977a29ce0
Mitchell Kim <mckim0928@gmail.com>
2017-03-05 15:52:38 -0500

    work-in-progress hero page

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import classes from './HeroPage.scss'
-
+import { Col, Row } from 'react-bootstrap'
 export const HeroPage = () => (
   <div>
     <div className={classes.hero}>
-      <div className={classes.mainText}>
-        <h1 className={classes.header}>IDEATE</h1>
-        <h3 className={classes.subheader}>The student innovation & design conference <br />
-            April 15, 2017 @ Duke University </h3>
+      <Row clasName={classes.headerSection}>
+        <Col md={9}>
+          <div className={classes.header}>IDEATE</div>
+          <div className={classes.subheader}>
+              The student innovation & design conference <br />
+              April 15, 2017 @ Duke University
+          </div>
+        </Col>
+        <Col md={3}>
+           <img className={classes.lightbulb} src='lightbulb.png' alt='placeholder' />
+        </Col>
+      </Row>
+      <Row>
         <div className={classes.buttonDiv}>
           <a className={classes.registerLink} eventKey={1} href={'http://my.hackduke.org/login'}>
             <button className={classes.registerButton}> Register </button>
           </a>
         </div>
-      </div>
+      </Row>
     </div>
   </div>
 )
