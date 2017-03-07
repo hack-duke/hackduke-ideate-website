37c007f5d8fc3fd3d5c947af844fed5708ea4b0c
Mitchell Kim <mckim0928@gmail.com>
2017-03-06 23:35:23 -0500

    fixing linting errors and committing current progress that is mergable and displayable to the group. need to get rid of all the hardcoded transform values

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -1,33 +1,31 @@
 import React from 'react'
 import classes from './HeroPage.scss'
-import { Col, Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
 export const HeroPage = () => (
   <div>
     <div className={classes.hero}>
       <div className={classes.videoBlock}>
-        <video preload="preload" className={classes.video} autoPlay loop>
-          <source src="Ideate2k17.mp4" type="video/mp4"></source>
+        <video preload='preload' className={classes.video} autoPlay loop>
+          <source src='Ideate2k17.mp4' type='video/mp4'></source>
         </video>
       </div>
-      <Row clasName={classes.headerSection}>
-        <Col md={9}>
+      <div clasName={classes.headerSection}>
+        <Col md={10}>
           <div className={classes.header}>IDEATE</div>
           <div className={classes.subheader}>
             The student innovation & design conference <br />
             April 15, 2017 @ Duke University
           </div>
         </Col>
-        <Col md={3}>
+        <Col md={2}>
           <img className={classes.lightbulb} src='lightbulb.png' alt='placeholder' />
         </Col>
-      </Row>
-      <Row>
-        <div className={classes.buttonDiv}>
-          <a className={classes.registerLink} eventKey={1} href={'http://my.hackduke.org/login'}>
-            <button className={classes.registerButton}> Register </button>
-          </a>
-        </div>
-      </Row>
+      </div>
+      <div className={classes.buttonDiv}>
+        <a className={classes.registerLink} eventKey={1} href={'http://my.hackduke.org/login'}>
+          <button className={classes.registerButton}> Register </button>
+        </a>
+      </div>
     </div>
   </div>
 )
