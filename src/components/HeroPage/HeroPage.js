ef4e9f61769d8fb3e823edf87d8f3e705333967c
Mitchell Kim <mckim0928@gmail.com>
2017-03-21 15:35:27 -0400

    fix lint errors

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import classes from './HeroPage.scss'
-import { Col, Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
 
 export const HeroPage = () => (
   <div>
@@ -10,18 +10,18 @@ export const HeroPage = () => (
           <source src='Ideate2k17.mp4' type='video/mp4'></source>
         </video>
       </div>
-        <div className={classes.headerSection}>
-          <Col md={10}>
-            <div className={classes.header}>IDEATE</div>
-            <div className={classes.subheader}>
-              The student innovation & design conference <br />
-              April 15, 2017 @ Duke University
-            </div>
-          </Col>
-          <Col md={2}>
-            <img className={classes.lightbulb} src='lightbulb.png' alt='placeholder' />
-          </Col>
-        </div>
+      <div className={classes.headerSection}>
+        <Col md={10}>
+          <div className={classes.header}>IDEATE</div>
+          <div className={classes.subheader}>
+            The student innovation & design conference <br />
+            April 15, 2017 @ Duke University
+          </div>
+        </Col>
+        <Col md={2}>
+          <img className={classes.lightbulb} src='lightbulb.png' alt='placeholder' />
+        </Col>
+      </div>
       <div className={classes.buttonDiv}>
         <a className={classes.registerLink} href={''}>
           <button className={classes.registerButton}> Register Soon </button>
